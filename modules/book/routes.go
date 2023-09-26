31d8f547da9b9dc553cc8e4c49865a7c3e16c520
randomize721 <anggadp91@gmail.com>
2023-09-26 13:08:08 +0800

    mindahin routes

--- modules/book/routes.go
+++ modules/book/routes.go
@@ -4,8 +4,8 @@ import "github.com/gofiber/fiber/v2"
 
 func Setup(parent fiber.Router) {
 	parent.Get("/", GetAllBooks)
-	parent.Post("/", AddBook)
 	parent.Get("/:id<string>", GetBookById)
+	parent.Post("/", AddBook)
 	parent.Put("/:id<string>", UpdateBookById)
 	parent.Delete("/:id<string>", DeleteBookById)
 }
